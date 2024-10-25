e54998b979f81e9e00292c8756ea9418fca2f722
nathanstephenson <nathans@invevo.com>
2024-10-25 21:44:08 +0100

    add grid and game state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import { GameStateProvider } from "./game-state"
 import { TelescopeMiniGame } from "./TelescopeMiniGame"
 import BackgroundMusic from "./audio/BackgroundMusic.tsx"
 import { Grid } from "./map/grid"
 
 function App() {
     return (
-        <>
+        <GameStateProvider>
             <BackgroundMusic />
             <Grid />
             <TelescopeMiniGame />
-        </>
+        </GameStateProvider>
     )
 }
 
--- src/map/grid.tsx
+++ src/map/grid.tsx
@@ -1,26 +1,35 @@
-export const Grid = () => {
-  const width = 10;
-  const height = 10;
-  return (
-    <div className="flex flex-row grow">
-      {new Array(width * height).fill(1).map((_, i) => (
-        <Cell key={i} x={Math.floor(i / width)} y={Math.floor(i / height)} />
-      ))}
-    </div>
-  );
-};
+import { useGameState } from "../game-state"
 
-const Cell = ({ x, y }: { x: number; y: number }) => {
-  console.log("I am a cell");
-  const onCellClicked = () => {
-    console.log(`Cell clicked at x: ${x}, y: ${y}`);
-  };
-  return (
-    <div
-      onClick={onCellClicked}
-      className="size-32 bg-white border border-white"
-    >
-      This is a cell
-    </div>
-  );
-};
+export const Grid = () => {
+    const width = 4
+    const height = 10
+    return (
+        <div className="flex flex-col grow">
+            {new Array(height).fill(1).map((_, y) => (
+                <div className="flex flex-row">
+                    {new Array(width).fill(1).map((_, x) => {
+                        const xCord = x + 1
+                        const yCord = y + 1
+                        const idx = xCord * yCord
+                        return <Cell key={idx} x={xCord} y={yCord} index={idx} />
+                    })}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+const Cell = ({ x, y, index }: { x: number; y: number; index: number }) => {
+    const { grid } = useGameState()
+
+    const onClickCell = () => {
+        grid.x.set(x)
+        grid.y.set(y)
+    }
+
+    return (
+        <div onClick={onClickCell} className="size-24 bg-red border border-red hover:bg-zinc-50">
+            This is a cell
+        </div>
+    )
+}
