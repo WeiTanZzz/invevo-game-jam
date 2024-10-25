7b0a77c33d1828ce84d4945a8fd1e4b740cb0c2e
wei <wei@invevo.com>
2024-10-25 21:31:05 +0100

    add bg music

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { TelescopeMiniGame } from "./TelescopeMiniGame"
+import BackgroundMusic from "./audio/BackgroundMusic.tsx"
 import { Grid } from "./map/grid"
 
 function App() {
     return (
         <>
+            <BackgroundMusic />
             <Grid />
             <TelescopeMiniGame />
         </>
