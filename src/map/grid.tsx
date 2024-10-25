23c574c9db94cb8695eee1de6b1626b2dd56a45c
nathanstephenson <nathans@invevo.com>
2024-10-25 23:03:48 +0100

    move with wasd

--- src/map/grid.tsx
+++ src/map/grid.tsx
@@ -1,14 +1,30 @@
+import { useLayoutEffect } from "react"
 import { useGameState } from "../game-state"
 import { cn } from "../util"
 
+export const GRID_WIDTH = 10
+export const GRID_HEIGHT = 5
+
 export const Grid = () => {
-    const width = 4
-    const height = 10
+    const { grid } = useGameState()
+
+    const onKeyPress = (e: KeyboardEvent) => {
+        if (e.key === "a") grid.x.increment(-1)
+        if (e.key === "d") grid.x.increment(1)
+        if (e.key === "w") grid.y.increment(-1)
+        if (e.key === "s") grid.y.increment(1)
+    }
+
+    useLayoutEffect(() => {
+        window.addEventListener("keydown", onKeyPress)
+        return () => window.removeEventListener("keydown", onKeyPress)
+    }, [])
+
     return (
-        <div className="flex flex-col grow">
-            {new Array(height).fill(1).map((_, y) => (
+        <div className="flex flex-col grow items-center justify-center">
+            {new Array(GRID_HEIGHT).fill(1).map((_, y) => (
                 <div className="flex flex-row">
-                    {new Array(width).fill(1).map((_, x) => {
+                    {new Array(GRID_WIDTH).fill(1).map((_, x) => {
                         const xCord = x + 1
                         const yCord = y + 1
                         const idx = xCord * yCord
@@ -38,9 +54,9 @@ const Cell = ({ x, y }: { x: number; y: number }) => {
         <div
             onClick={onClickCell}
             className={cn(
-                "size-24 items-center justify-center border border-red hover:bg-zinc-50",
-                isCurrentCell && "border-yellow-900",
-                canMoveToCell && "hover:bg-green-200"
+                "size-20 items-center justify-center border",
+                isCurrentCell && "border-yellow-500",
+                canMoveToCell ? "hover:border-green-500" : !isCurrentCell && "hover:border-red-500"
             )}
         >
             {isCurrentCell && <span>🚀</span>}
