726b71f04824951199d88f1ef72d78c44e8bb451
rozz120 <32567639+rozz120@users.noreply.github.com>
2024-10-25 21:52:49 +0100

    make the telescope game bearable

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react"
 import { TelescopeMiniGame } from "./TelescopeMiniGame"
 import BackgroundMusic from "./audio/BackgroundMusic.tsx"
 import { Grid } from "./map/grid"
 
 function App() {
+    const [inTelescopeMiniGame, setInTelescopeMiniGame] = useState(false)
     return (
         <>
-            <BackgroundMusic />
-            <Grid />
-            <TelescopeMiniGame />
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                onClick={() => setInTelescopeMiniGame(!inTelescopeMiniGame)}
+            >
+                {inTelescopeMiniGame ? "exit telescop mini game" : "enter Telescope Mini Game "}
+            </button>
+            {inTelescopeMiniGame ? (
+                <TelescopeMiniGame />
+            ) : (
+                <>
+                    <BackgroundMusic />
+                    <Grid />
+                </>
+            )}
         </>
     )
 }
