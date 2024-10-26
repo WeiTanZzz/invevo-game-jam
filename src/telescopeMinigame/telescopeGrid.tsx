17c18cf9065a1773b99a913ac44e9bd41a1038b0
rozz120 <32567639+rozz120@users.noreply.github.com>
2024-10-26 16:04:11 +0100

    fix grid scaling

--- src/telescopeMinigame/telescopeGrid.tsx
+++ src/telescopeMinigame/telescopeGrid.tsx
@@ -8,15 +8,15 @@ export const TelescopeGrid = () => {
     const { islands } = useGameState()
 
     return (
-        <div className="flex flex-col grow pointer-events-auto">
+        <div className="flex pointer-events-auto flex-1 flex-col items-center justify-center">
             {new Array(height).fill(1).map((_, y) => (
-                <div className="flex flex-row">
+                <div className="flex flex-row flex-1 grow w-full">
                     {new Array(width).fill(1).map((_, x) => {
                         const island = islands.get.find(island => island.gridPosition.x === x && island.gridPosition.y === y)
                         if (island) {
                             return <Cell imageToRender={island.path} />
                         } else {
-                            return <div className="size-24 bg-blue-600 items-center justify-center" />
+                            return <div className="bg-blue-600 w-1/12 grow aspect-square items-center justify-center " />
                         }
                     })}
                 </div>
@@ -27,7 +27,7 @@ export const TelescopeGrid = () => {
 
 const Cell = ({ imageToRender }: { imageToRender: string }) => {
     return (
-        <div className={cn("size-24 bg-blue-600 items-center justify-center")}>
+        <div className={cn("bg-blue-600 w-1/12 grow aspect-square items-center justify-center")}>
             {
                 <span>
                     <img className="bg-cover bg-" src={imageToRender} alt={imageToRender} />
