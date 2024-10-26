d413821b5efa7f9df4f201f4dc75da05ab098fe7
nathanstephenson <nathans@invevo.com>
2024-10-26 23:10:30 +0100

    fix overlay blocking click

--- src/overlay/overlay.tsx
+++ src/overlay/overlay.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react"
 
 export const Overlay = ({ children }: { children: ReactNode }) => {
-    return <div className="text-left z-10 absolute w-full h-full ml-10 flex items-start">{children}</div>
+    return <div className="text-left pointer-events-none z-10 absolute w-full h-full ml-10 flex items-start">{children}</div>
 }
