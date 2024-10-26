8005c0fabf8a0413042598144481f99d425980d6
Jamie <1936823+JWroe@users.noreply.github.com>
2024-10-26 15:22:50 +0100

    added a speech bubble

--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,4 +1,5 @@
 import { Grid } from "./grid"
+import { Speech } from "./speech"
 
 export const Map = () => {
     return (
@@ -6,6 +7,7 @@ export const Map = () => {
             className="flex grow bg-contain h-full w-full bg-no-repeat bg-center items-center justify-center bg-blue-200 overflow-hidden"
             style={{ backgroundImage: 'url("./ship.png")' }}
         >
+            <Speech />
             <div className="w-full h-full py-[11%] ps-[10%] pe-[22%]">
                 <Grid />
             </div>
