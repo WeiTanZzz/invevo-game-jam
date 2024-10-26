888a1daa94b423bb228cd5d1afc03e8b21c7f080
rozz120 <32567639+rozz120@users.noreply.github.com>
2024-10-26 19:01:46 +0100

    make the legend more legendary

--- src/mini-games/telescopeMinigame/TelescopeMiniGame.tsx
+++ src/mini-games/telescopeMinigame/TelescopeMiniGame.tsx
@@ -21,8 +21,9 @@ export const TelescopeMiniGame = () => {
 }
 const UseMap = ({ islandToFind }: { islandToFind: IslandState }) => {
     return (
-        <div className="flex flex-col items-center justify-center ">
+        <div className="flex flex-col items-center justify-center p-6 bg-[#f4e4c1] text-[#4a2c0a] font-serif rounded-lg shadow-lg border-2 border-[#d2b48c] relative overflow-hidden">
             <img src={islandToFind.path} alt="map" className="h-1/3 w-1/3" />
+            <p className="text-lg">Ahoy! This be {islandToFind.name}</p>
         </div>
     )
 }
@@ -34,7 +35,7 @@ const UseTelescopeMiniGame = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
     useEffect(() => {
-        activeSpeechBubble.set("Use your telescope to find the Island, use the legend for assistance.")
+        activeSpeechBubble.set(`Use your telescope to find ${islandToFind.name}, use the legend for assistance.`)
 
         audio.addEffect(AUDIO_FIND_ISLAND, "/audio/effect/find-island.mp3")
 
