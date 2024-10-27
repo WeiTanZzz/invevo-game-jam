6b00a747d4b8447a34f2cd833f27f8ae61d9744e
rozz120 <32567639+rozz120@users.noreply.github.com>
2024-10-27 17:03:38 +0000

    change captain

--- src/overlay/speech.tsx
+++ src/overlay/speech.tsx
@@ -5,8 +5,8 @@ export const Speech = () => {
 
     return (
         <div className="flex flex-row">
-            <div className="mr-4 text-center">
-                <img src="./captain.png" className="w-52 h-20" alt="Captain" />
+            <div className="mr-4 flex flex-col items-center text-center">
+                <img src="./spooky_captain.png" className="w-28 h-28" alt="Captain" />
                 <span className=" text-lg font-serif italic text-orange-600 mt-2 block">Captain Ghost Beard</span>
             </div>
             <div className="flex h-fit bg-[#f4e4c1] p-4 text-[#4a2c0a] font-serif rounded-lg shadow-lg border-2 border-[#d2b48c] relative overflow-hidden">
